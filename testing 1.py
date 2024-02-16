4b6dc1f7c03d89c9811e3ddb619003fa5f13b82f
sanudageenaka <160229358+sanudageenaka@users.noreply.github.com>
2024-02-16 22:09:43 +0530

    Add files via upload
    Git Repo link to the Login UI

---
+++ testing 1.py
@@ -0,0 +1,91 @@
+#question(a)
+num = 5
+print(num) #Output? #5
+num = num + 2
+print(num) #Output? #7
+num = num // 3 * 6
+print(num) #Output? #12
+print(7 + 15 % 4) #Output? #10
+num = 24 // 3 // 4
+print(num) #Output?#2
+#question(b)(1)
+a = "Hello out there."
+print(a)
+b = 'Hello again.'
+print(b)
+X = a+b
+print(X)
+#question(b)(2)
+total = str('10')
+greet = 'Hello'
+both = total + greet
+print(both)
+#question(b)(3)
+print("A", end = ' ')
+print("B", end = ' ')
+print("C", end = ' ')
+print("D", end = ' ')
+#A B C D
+#question(b)(4)
+a = '10'
+b = '99'
+c = a + b
+print(c)#1099
+#modify program for value 109
+a=10
+b=99
+c=a+b
+print(c)
+#question(4)(1)
+name = input('Please enter your name: \n')
+print('Hello', name)
+#question(4)(2)
+age = input('Please enter your age: \n')
+print("your age is", age)
+#question(4)(4)
+the_text = input('Enter some text.\n') # get some text!
+#print - version 1
+print('This is what you entered: ')
+print(the_text)
+#print – version 2
+print('This is what you entered:', the_text)
+#print - version 3 - Supress printing of a new line, use end=' '
+print('This is what you entered:', end=' ')
+print(the_text)
+#three print statements are same
+
+#question(4)(5)(2)
+num_1 = int(input('Enter number one: '))
+num_2 = int(input('Enter number two: '))
+total = num_1 + num_2
+print(total)
+
+#question(4)(5)(3)
+cost = float(input('Enter cost of item: '))
+paid = float(input('Enter cash paid: '))
+change = paid - cost
+print('Change is: ', change)
+#question(4)(5)(4)
+num_1 = float(input('Enter number one: '))
+num_2 = float(input('Enter number two: '))
+num_3 = float(input('Enter number three: '))
+average = (num_1 + num_2 + num_3) / 3
+print('The average is:', average)
+print(round(average,2))            
+#question(4)(5)(5)correct code
+# shopping program with 4 bugs
+a=3 # number of apples
+o=0 # number of oranges
+print('you have: ',a,'apples and',o,'oranges')
+buy_a=4 # apples you buy
+buy_o=6 # oranges you buy
+print('you buy: ',buy_a,'apples and',buy_o,'oranges')
+a=a+buy_a
+o=o+buy_o
+print('you now have:',a,'apples and',o,'oranges')
+#additional exercises(6) 
+c = float(input('Enter centigrade value: '))
+f=(c*1.8)
+print("farenhite",f)
+#additional exercises(h)
+
